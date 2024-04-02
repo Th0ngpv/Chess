fb72ec6c92a4ca1c934b63a93985953621397714
Thong <117352680+Th0ngpv@users.noreply.github.com>
2024-04-02 11:13:36 +0700

    add legal move array and function to check the move of pawns but didn't work

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ let isWhiteTurn = true;
 const board = document.querySelectorAll(".square");
 const pieces = document.querySelectorAll(".piece");
 const piecesImages = document.querySelectorAll("img");
-
-setupBoardSquares();
-setupPieces();
 //setup the board square
 function setupBoardSquares() {
     //add drag and drop event listener
@@ -44,10 +41,9 @@ function drag(event) {
     //check who turn is it 
     if ((isWhiteTurn && pieceColor === "white")||(!isWhiteTurn && pieceColor === "black")) {
         event.dataTransfer.setData("text",piece.id);
-        // const startingSquareId = piece.parentNode.id;
-        // getPossibleMoves(startingSquareId,piece);
+        const startingSquareId = piece.parentNode.id;
+        getPossibleMoves(startingSquareId,piece);
     }
-    
 }
 //get the piece id and location then append it to the current drop target
 function drop(event) {
@@ -56,14 +52,17 @@ function drop(event) {
     const piece = document.getElementById(data);
     const destinationSquare = event.currentTarget;
     let destinationSquareId = destinationSquare.id;
-    //check if the destinationSquare is occupied and is it the same color
-    if (isSquareOccupied(destinationSquare) === "blank") {
+    //check if the destinationSquare is occupied and is it in the legalSquare array
+    if ((isSquareOccupied(destinationSquare) === "blank")  && (legalSquares.includes
+        (destinationSquareId)) ) {
         destinationSquare.appendChild(piece);
         //switch turn
         isWhiteTurn = !isWhiteTurn;
+        legalSquares.length = 0;
         return;
     }
-    if ((isSquareOccupied(destinationSquare) !== "blank")) {
+    if ((isSquareOccupied(destinationSquare) !== "blank") && (legalSquares.includes
+        (destinationSquareId)) ) {
         //remove the piece inside
         while (destinationSquare.firstChild) {
             destinationSquare.removeChild(destinationSquare.firstChild);
@@ -71,6 +70,7 @@ function drop(event) {
         destinationSquare.appendChild(piece);
         //switch turn
         isWhiteTurn = !isWhiteTurn;
+        legalSquares.length = 0;
         return;
     }
 }
@@ -83,3 +83,82 @@ function isSquareOccupied(square) {
         return "blank";
     }
 }
+//get all legal moves for a piece
+function getPossibleMoves(startingSquareId,piece) {
+    const pieceColor = piece.getAttribute("color");
+    if (piece.classList.contains("pawn")) {
+        getPawnMoves(startingSquareId,pieceColor);
+    }
+}
+//check pawn legal moves
+function getPawnMoves(startingSquareId,pieceColor) {
+    checkPawnDiagonalCaptures(startingSquareId,pieceColor);
+    checkPawnForwardMoves(startingSquareId,pieceColor);
+}
+//check pawn diagonal capture
+function checkPawnDiagonalCaptures(startingSquareId,pieceColor) {
+    const file = startingSquareId.charAt(0);
+    const rank = startingSquareId.charAt(1);
+    const rankNumber = parseInt(rank);
+    let currentFile = file;
+    let currentRank = rankNumber;
+    let currentSquareId = currentFile + currentRank;
+    let currentSquare = document.getElementById(currentSquareId);
+    let squareContent = isSquareOccupied(currentSquare);
+    const direction = pieceColor == "white" ? 1:-1;
+
+    currentRank += direction;
+    for (let i = 0; i <= 1; i += 2){
+        currentFile = String.fromCharCode(file.charCodeAt(0) + i);
+        //check if the move is inside the board
+        if (currentFile >= "a" && currentFile <= "h") {
+            currentSquareId = currentFile + currentRank;
+            currentRank = document.getElementById(currentSquareId);
+            squareContent = isSquareOccupied(currentSquare);
+            //check if the square is occupied 
+            if (squareContent !== "blank" && squareContent !== pieceColor) {
+                legalSquares.push(currentSquareId);
+            }
+        }
+    }
+}
+//check pawn forward moves
+function checkPawnForwardMoves(startingSquareId,pieceColor) {
+    const file = startingSquareId.charAt(0);
+    const rank = startingSquareId.charAt(1);
+    const rankNumber = parseInt(rank);
+    let currentFile = file;
+    let currentRank = rankNumber;
+    let currentSquareId = currentFile + currentRank;
+    let currentSquare = document.getElementById(currentSquareId);
+    let squareContent = isSquareOccupied(currentSquare);
+    const direction = pieceColor == "white" ? 1:-1;
+
+    currentRank += direction;
+    currentSquareId = currentFile + currentRank;
+    currentRank = document.getElementById(currentSquareId);
+    squareContent = isSquareOccupied(currentSquare);
+    //check if the square in front of it is occupied 
+    if (squareContent !== "blank") {
+        return;
+    }
+    legalSquares.push(currentSquareId);
+    //check if the pawn is at the 2nd or 7th rank or not
+    if (rankNumber !== 2 && rankNumber !== 7) {
+        return;
+    }
+    currentRank += direction;
+    currentSquareId = currentFile + currentRank;
+    currentRank = document.getElementById(currentSquareId);
+    squareContent = isSquareOccupied(currentSquare);
+    //check if the square in front of it is occupied 
+    if (squareContent !== "blank") {
+        return;
+    }
+    legalSquares.push(currentSquareId);
+}
+
+
+//calling function
+setupBoardSquares();
+setupPieces();
